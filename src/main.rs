e9c9ffbc6e8e41d9569b3f590c0e90d802b378d7
ayxan <ayxan.20145@gmail.com>
2026-06-14 21:41:49 +0400

    added swww.rs and swaybg.rs but they dont do anything yet

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,11 @@
-use colored::Colorize;
-use crossterm::{
-    terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode},
-    execute,
-    cursor::MoveTo,
-    style::Print,
-    event::{self, Event, KeyCode},
-};
-use std::io::{self};
+use std::io;
+
+mod menu;
+mod swww;
+mod swaybg;
 
 fn main() -> io::Result<()> {
-    enable_raw_mode()?;
+    menu::run()?;
 
-    let mut stdout = io::stdout();
-
-    let mut selected: usize = 0;
-
-    MoveTo(0, 0);
-
-    loop {
-        execute!(stdout, MoveTo(0, 0), Clear(ClearType::All))?;
-
-        if selected == 0 {
-            let name = "swww";
-            let other_name = "swaybg";
-            execute!(stdout, Print(format!("> {}\r\n", name.blue())))?;
-            execute!(stdout, Print(format!("  {}\r\n", other_name.blue())))?;
-        } else {
-            let name = "swaybg";
-            let other_name = "swww";
-
-            execute!(stdout, Print(format!("  {}\r\n", other_name.blue())))?;
-            execute!(stdout, Print(format!("> {}\r\n", name.blue())))?;
-        }
-
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('j') | KeyCode::Down => {
-                    if selected < 1 {
-                        selected += 1;
-                    }
-                }
-                KeyCode::Char('k') | KeyCode::Up => {
-                    if selected > 0 {
-                        selected -= 1;
-                    }
-                }
-                KeyCode::Char('q') => {
-                    break;
-                }
-
-                _ => {}
-            }
-        }
-    }
-
-    disable_raw_mode()?;
     Ok(())
 }
---
+++ src/menu.rs
@@ -0,0 +1,68 @@
+use crate::{swaybg, swww};
+
+use colored::Colorize;
+use crossterm::{
+    terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode},
+    execute,
+    cursor::MoveTo,
+    style::Print,
+    event::{self, Event, KeyCode},
+};
+use std::io;
+
+pub fn run() -> io::Result<()> {
+    enable_raw_mode()?;
+
+    let mut stdout = io::stdout();
+
+    let mut selected: usize = 0;
+
+    let entries = ["swww", "swaybg"];
+
+    loop {
+        execute!(stdout, MoveTo(0, 0), Clear(ClearType::All))?;
+
+        for (i, name) in entries.iter().enumerate() {
+            if i == selected {
+                execute!(stdout, Print(format!("> {}\r\n", name.blue())))?;
+            } else {
+                execute!(stdout, Print(format!("  {}\r\n", name.blue())))?;
+            }
+        }
+
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('j') | KeyCode::Down => {
+                    if selected + 1 < entries.len() {
+                        selected += 1;
+                    }
+                }
+                KeyCode::Char('k') | KeyCode::Up => {
+                    if selected > 0 {
+                        selected -= 1;
+                    }
+                }
+                KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
+                    if selected == 0 {
+                        disable_raw_mode()?;
+                        swww::swww::run()?;
+                        enable_raw_mode()?;
+                    } else {
+                        disable_raw_mode()?;
+                        swaybg::swaybg::run()?;
+                        enable_raw_mode()?;
+                    }
+                }
+                KeyCode::Char('q') => {
+                    break;
+                }
+
+                _ => {}
+            }
+        }
+    }
+
+    disable_raw_mode()?;
+    Ok(())
+}
+
